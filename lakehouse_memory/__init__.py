d8ba7cb3cfe0b40f391d5b164ae24e93b232acbb
Travis Burmaster <travis@burmaster.com>
2026-05-20 14:34:29 -0400

    refactor: simplify public API by relying on __all__ instead of runtime cleanup

--- lakehouse_memory/__init__.py
+++ lakehouse_memory/__init__.py
@@ -2,9 +2,6 @@
 
 from __future__ import annotations
 
-import sys
-from types import ModuleType
-
 from lakehouse_memory.config import EmbeddingConfig, MemoryConfig
 from lakehouse_memory.memory import Memory
 from lakehouse_memory.scope import Scope
@@ -12,12 +9,3 @@ from lakehouse_memory.scope import Scope
 __version__ = "0.1.0a0"
 
 __all__ = ["EmbeddingConfig", "Memory", "MemoryConfig", "Scope", "__version__"]
-
-# Clean up namespace to prevent internal modules from leaking
-_current_module = sys.modules[__name__]
-_public_names = set(__all__)
-for _name in list(vars(_current_module).keys()):
-    if _name.startswith("_") or _name in _public_names:
-        continue
-    if isinstance(vars(_current_module).get(_name), ModuleType):
-        delattr(_current_module, _name)
